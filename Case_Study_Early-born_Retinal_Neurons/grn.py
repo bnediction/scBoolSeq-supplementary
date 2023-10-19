cd241d2d9e28022857e6eb99e7e9fca49dc9898d
Gustavo Magaña López <28574085+gmagannaDevelop@users.noreply.github.com>
2023-10-20 01:49:10 +0200

    :sparkles: Added notebooks.
    .

--- Case_Study_Early-born_Retinal_Neurons/grn.py
+++ Case_Study_Early-born_Retinal_Neurons/grn.py
@@ -151,13 +151,14 @@ def extract_largest_wcc(grn: nx.MultiDiGraph) -> nx.MultiDiGraph:
     """
     return nx.subgraph(grn, max(nx.weakly_connected_components(grn), key=len))
 
-
 def colorFader(c1, c2, mix=0):
     """fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
     taken from:
     https://stackoverflow.com/questions/25668828/how-to-create-colour-gradient-in-python
     """
     c1 = np.array(mpl.colors.to_rgb(c1))
+    c2 = np.array(mpl.colors.to_rgb(c2))
+    return mpl.colors.to_hex((1 - mix) * c1 + mix * c2) 
 
 def color_influence_graph(ig, mode="influence", cold="lightblue", hot="yellow", scores=None):
     """
